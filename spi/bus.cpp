b4f72bd69a4b9597ad5bc7519329231957a15c46
Bas Visscher <bas@koolecontrols.nl>
2022-12-07 13:22:35 +0100

    Support for DMA

--- spi/bus.cpp
+++ spi/bus.cpp
@@ -1,7 +1,7 @@
 #include "bus.h"
 #include "string.h"
 
-esp_err_t ESP_Com::SPIBus::Init(spi_host_device_t host, gpio_num_t clk, gpio_num_t mosi, gpio_num_t miso, gpio_num_t qwp, gpio_num_t qhd)
+esp_err_t ESP_Com::SPIBus::Init(spi_host_device_t host, gpio_num_t clk, gpio_num_t mosi, gpio_num_t miso, gpio_num_t qwp, gpio_num_t qhd, spi_dma_chan_t dma)
 {
 	spi_bus_config_t spiConfig;
 	memset(&spiConfig, 0, sizeof(spi_bus_config_t));
@@ -10,7 +10,8 @@ esp_err_t ESP_Com::SPIBus::Init(spi_host_device_t host, gpio_num_t clk, gpio_num
 	spiConfig.miso_io_num = miso;
 	spiConfig.quadwp_io_num = qwp;
 	spiConfig.quadhd_io_num = qhd;
+	spiConfig.max_transfer_sz = 1024;
 	_host = host;
-	return spi_bus_initialize(host, &spiConfig, 0);
+	return spi_bus_initialize(host, &spiConfig, dma);
 }
 
